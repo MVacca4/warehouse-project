879d1c34ca84675edbf1c34b7c7e85bd08e71bbb
Massimiliano Vacca <145068543+MVacca4@users.noreply.github.com>
2023-11-19 15:23:48 -0700

    Update Warehouse.java
    Finished coding viewEmployee method.
    Started adding comments in the main method.

--- WarehouseProject/src/warehouse/app/Warehouse.java
+++ WarehouseProject/src/warehouse/app/Warehouse.java
@@ -1,55 +1,52 @@
-package warehouse.app;
-import java.util.*;
+import java.util.ArrayList;
+import java.util.List;
+import java.util.Scanner;
 
+/**
+ * The base class for the Warehouse management program.
+ * @author Massimiliano Vacca
+ **/ 
 public class Warehouse {
 	
-	static List<Employee> employeeData = new ArrayList<Employee>();
+	String name = ""; 						// Used when an employee's name is needed in methods.
+	Scanner sc = new Scanner(System.in); 				// Takes user input.
+	List<Employee> employeeData = new ArrayList<Employee>();	// Container for holding employee data.
 	
 	public static void main(String[] args) {
 		
-		boolean end = false; // False keeps program running, true ends program.
+		Warehouse wh = new Warehouse();				// Warehouse object to call methods with.
+		boolean end = false; 					// False keeps program running, true ends program.
 		
+		// Main menu loop, continues until the user enters 0, ending the program.
 		while (end == false) {
 		
-			Scanner sc = new Scanner(System.in);
-			int choice = 0;
-			optionDisplay();
-			choice = sc.nextInt();
+			Scanner mainSc = new Scanner(System.in);
+			int choice = 0;					// Holds choice for main menu.
+			wh.optionDisplay();				// Display main menu options.
+			choice = mainSc.nextInt();			// Get choice selection from user.
 			
+			// Call method based on which choice the user made, or end the program if 0 was selected.
 			switch (choice) {
 				case 0:
 					end = true;
 					break;
 				case 1:
-					createEmployee();
+					wh.createEmployee();
 					break;
 				case 2:
-					deleteEmployee();
+					wh.deleteEmployee();
 					break;
 				case 3:
-					viewEmployee();
+					wh.viewEmployee();
 					break;
 				default:
 			}
 		}
 	}
 	
-	public static void optionDisplay() {
+	public void createEmployee() {
 		
-		System.out.print("Welcome to the warehouse database system. ");
-		System.out.println("Please type the corresponding number of your choice and hit enter:\n");
-		System.out.println("0. Exit program.");
-		System.out.println("1. Create new employee.");
-		System.out.println("2. Delete existing employee.");
-		System.out.println("3. View employee info.");
-	}
-	
-	public static void createEmployee() {
-		
-		Scanner sc = new Scanner(System.in);
-		String choice = "";
-		String name = "";
-		String position = "";
+		sc = new Scanner(System.in);
 		int salary = 0;
 		float hours = 0;
 	
@@ -57,7 +54,7 @@ public class Warehouse {
 		System.out.print("Enter employee's name: ");
 		name = sc.nextLine();
 		System.out.print("Enter employee's position: ");
-		position = sc.nextLine();
+		String position = sc.nextLine();
 		
 		// Surround with try...catch in case user enters nothing for salary or hours.
 		try {
@@ -72,31 +69,39 @@ public class Warehouse {
 		Employee employeeInfo = new Employee(name, position, salary, hours);
 		employeeData.add(employeeInfo);
 		System.out.println("Employee entry created!\n-----------------------");
+		wait(1500);
 	}
 	
-	public static void deleteEmployee() {
+	public void deleteEmployee() {
 		
-		Scanner sc = new Scanner(System.in);
-		String name = "";
-		boolean loopEnd = false;
+		sc = new Scanner(System.in);
+		boolean end = false;
 		
-		while (loopEnd == false) {
-			
-			System.out.print("Enter the name of the employee you wish to delete, or enter 0 to go back to the Main Menu: ");
-			name = sc.nextLine();
-			
-			try {
-				if (Integer.parseInt(name) == 0) {loopEnd = true;} // If user entered 0, end while loop.
-			} catch (NumberFormatException e) {
-				// Loop through all employee entries and delete an entry if it matches the name typed.
-				for (int i = 0; i < employeeData.size(); i++) {
-					if (name.equalsIgnoreCase(employeeData.get(i).name)) {
-						employeeData.remove(i);
-						System.out.println("Employee entry has been deleted!\n");
-						break;
-					} else {
-						if (i == employeeData.size() - 1) {	// Display message if loop has gotten to last employee entry.
-							System.out.println("Sorry, that employee name doesn't match any in our records. Please ensure you entered the name correctly.\n");
+		while (end == false) {
+
+			if (employeeData.size() == 0) {
+				System.out.println("The employee list currently has no entries. Returning you to the main menu.\n--------------------------------");
+				end = true;
+				wait(1500);
+			} else {
+				System.out.println("Enter the name of the employee you wish to delete, or enter 0 to go back to the main menu: ");
+				name = sc.nextLine();
+				
+				try {
+					if (Integer.parseInt(name) == 0) {end = true;} // If user entered 0, end while loop.
+				} catch (NumberFormatException e) {
+					// Loop through all employee entries and delete an entry if it matches the name typed.
+					for (int i = 0; i < employeeData.size(); i++) {
+						if (name.equalsIgnoreCase(employeeData.get(i).name)) {
+							employeeData.remove(i);
+							System.out.println("Employee entry has been deleted!\n--------------------------------");
+							wait(1500);
+							break;
+						} else {
+							if (i == employeeData.size() - 1) {	// Display message if loop has gotten to last employee entry.
+								System.out.println("Sorry, that employee name doesn't match any in our records. Please ensure you entered the name correctly.\n");
+								wait(1500);
+							}
 						}
 					}
 				}
@@ -104,12 +109,67 @@ public class Warehouse {
 		}
 	}
 	
-	public static void viewEmployee() {
+	public void viewEmployee() {
 		
-		System.out.println("Choose between 1 and " + employeeData.size() + " to see information for the corresdponding employee:");
+		sc = new Scanner(System.in);
+		boolean end = false;
+		Scanner detectEnter = new Scanner(System.in);
 		
-		for (int i = 1; i <= employeeData.size(); i++) {			// FIGURE OUT INDEX ISSUE
-			System.out.println(i + "." + employeeData.get(i).name);
+		if (employeeData.size() == 0) {
+			System.out.println("There are no existing employee entries, returning you to the main menu.");
+			System.out.println("--------------------------------");
+			wait(2000);
+		} else {
+			while (end == false) {
+				System.out.println("Choose between 1 and " + employeeData.size() + " to see information for the corresdponding employee:");
+				wait(1500);
+				
+				for (int i = 0; i < employeeData.size(); i++) {
+					System.out.println(i + 1 + ". " + employeeData.get(i).name);
+				}
+				
+				try {
+					System.out.println("Enter 0 to go back to the main menu.");
+					int choice = sc.nextInt();
+					
+					if (choice == 0) {
+						end = true;
+					}
+					
+					for (int i = 0; i < employeeData.size(); i++) {
+						if (choice == 1 + employeeData.indexOf(employeeData.get(i))) {
+							System.out.println("Name: " + employeeData.get(i).name + "\nPosition: " + employeeData.get(i).position
+									+ "\nSalary: " + employeeData.get(i).salary + "\nHours: " + employeeData.get(i).hours);
+							wait(1500);
+							System.out.println("\nHit enter to see the list of employees again.");
+							if (detectEnter.hasNextLine()) {String s = detectEnter.nextLine();} // Detects enter key press.
+						}
+					}
+				} catch (Exception e) {
+					System.out.println("Invalid entry, please use numbers only\n--------------------------------");
+					sc = new Scanner(System.in);
+					wait(1500);
+				}
+			}
 		}
+	}
+	
+	public static void wait(int ms) {
+		
+		try {
+			Thread.sleep(ms);
+		} catch (InterruptedException e) {
+			Thread.currentThread().interrupt();
+		}
+	}
+	
+	public void optionDisplay() {
+		
+		System.out.print("Welcome to the warehouse database system. ");
+		System.out.println("Please type the corresponding number of your choice and hit enter:\n");
+		System.out.println("0. Exit program.");
+		System.out.println("1. Create new employee.");
+		System.out.println("2. Delete existing employee.");
+		System.out.println("3. View employee list.");
 	}
 }
