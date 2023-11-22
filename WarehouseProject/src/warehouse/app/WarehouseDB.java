f722728e78e6d54408cbaf2a00d03a44f69672da
Massimiliano Vacca <145068543+MVacca4@users.noreply.github.com>
2023-11-22 14:26:55 -0700

    Create WarehouseDB.java
    WarehouseDB class will connect to database.
    Also should contain methods for creating, deleting, and viewing employee data in the database.

---
+++ WarehouseProject/src/warehouse/app/WarehouseDB.java
@@ -0,0 +1,37 @@
+package warehouse.app;
+
+import java.sql.*;
+
+public class WarehouseDB {
+	
+	Connection connect;
+	
+	public void connect() {
+	
+		try {
+			connect = DriverManager.getConnection("jdbc:mysql://localhost:3306/employee_data", "root", "");
+		} catch (SQLException e) {
+			e.printStackTrace();
+		}
+	}
+	
+	public void createEmployee(String name, String position, int salary, float hours) {
+		
+		try {
+			
+			String query = "insert into employees (name, position, salary, hours) values (?, ?, ?, ?)";
+			PreparedStatement prepStmt = connect.prepareStatement(query);
+			prepStmt.setString(1, name);
+			prepStmt.setString(2, position);
+			prepStmt.setInt(3, salary);
+			prepStmt.setFloat(4, hours);
+			
+			prepStmt.execute();
+			
+			connect.close();
+			
+		} catch (SQLException e) {
+			System.err.println(e.getMessage());
+		}
+	}
+}
